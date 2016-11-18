6f9a47a5bfade116110a2fa29e130569a540c960
abdennour <mail@abdennoor.com>
2016-11-18 15:03:53 +0300

    - pretty release with 6 props

--- src/Battery.js
+++ src/Battery.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Styling from './Style.js';
 const Methods = [
- 'onCharging', 'onDisCharging', 'addCss', 'removeCss',
- 'getContainerSize', 'getContainerColors', 'getContainerStyle', 'getForeground'
+ 'onCharging', 'onDisCharging', 'styleDidMount', 'removeCss','styleDidUpdate'
 ];
 class Battery extends React.Component {
  constructor(props) {
@@ -27,20 +26,23 @@ class Battery extends React.Component {
   });
  }
 
- addCss() {
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = Styling(this.props, this.state.id);
-  css.id = `style-react-battery-manager-${this.state.id}`;
-  document.querySelector('head').appendChild(css);
+ styleDidMount() {
+  this.css = document.createElement("style");
+  this.css.type = "text/css";
+  this.styleDidUpdate();
+  //this.css.id = `style-react-battery-manager-${this.state.id}`;
+  document.querySelector('head').appendChild(this.css);
  }
-
+ styleDidUpdate() {
+  if(this.css)
+   this.css.innerHTML = Styling(this.props, this.state);
+ }
  removeCss() {
   let previous = document.querySelector(`#style-react-battery-manager-${this.state.id}`);
   previous && previous.remove();
  }
  componentDidMount() {
-  this.addCss();
+  this.styleDidMount();
   if (typeof navigator.getBattery === 'function') {
    navigator.getBattery().then((battery) => {
     this.setState({
@@ -51,79 +53,28 @@ class Battery extends React.Component {
    });
   }
  }
-
+ componentDidUpdate() {
+   this.styleDidUpdate();
+ }
  componentWillUnmount() {
   this.removeCss();
  }
- getContainerSize() {
-  let containerWith, containerHeight, padding;
-  if (this.props.width && this.props.height) {
-   containerWith = this.props.width;
-   containerHeight = this.props.height;
-  } else if(this.props.width){
-    containerWith = this.props.width;
-    containerHeight = parseInt(containerWith/3)
-  } else if (this.props.height) {
-    containerHeight = this.props.height;
-    containerWith = 3 * containerHeight;
-  } else {
-   let sizeMap = {
-    small: [36, 12],
-    normal: [72, 24],
-    large: [108, 36],
-    xlarge: [144, 48],
-    xxlarge: [156, 60]
-   };
-   containerWith = sizeMap[this.props.size][0];
-   containerHeight = sizeMap[this.props.size][1];
-
-  }
-
-  return {
-   width: containerWith,
-   height: containerHeight,
-   padding: parseInt(containerHeight / 12)
-  };
- }
- getForeground() {
-  if(this.state.level && this.state.level <10) {
-    return this.props.colorOnCritical;
-  } else {
-    return (!this.state.charging) ? this.props.foreground : this.props.colorOnCharing;
-  }
- }
- getContainerColors() {
-  return {
-   background: this.props.background,
-   border: `1px solid ${this.getForeground()}`
-  }
- }
-
- getContainerStyle() {
-  return Object.assign({}, this.getContainerSize(), this.getContainerColors());
- }
- getBarStyle() {
-  return {
-   width: `${this.state.level}%`,
-   background: this.getForeground()
-  };
- }
 
  render() {
-
+//  this.styleDidUpdate();
   return (
-    <div className={"battery_"+this.state.id} style={this.getContainerStyle()}>
-     <div className={"level_"+this.state.id} style={this.getBarStyle()}></div>
+    <div className={"battery_"+this.state.id+((this.state.charging)?" plugged":"")}>
+     <div className={"level_"+this.state.id}></div>
     </div>
   )
  }
 }
 
 Battery.defaultProps = {
-
- size: 'large',
- background: 'white',
- foreground: 'black',
+ ltr:true,
+ size: 'normal',
+ background: 'transparent',
+ color: 'black',
  colorOnCharing: 'green',
  colorOnCritical:'red'
 };
--- src/Style.js
+++ src/Style.js
@@ -1,49 +1,143 @@
-module.exports = function(props, id) {
+const api = {
+ containerScale(props, state) {
+  return (props.ltr) ? 0.5 : -0.5;
+ },
+ containerSize(props, state) {
+  let containerWith, containerHeight, padding;
+  if (props.width && props.height) {
+   containerWith = props.width;
+   containerHeight = props.height;
+  } else if (props.width) {
+   containerWith = props.width;
+   containerHeight = parseInt(containerWith / 3)
+  } else if (props.height) {
+   containerHeight = props.height;
+   containerWith = 3 * containerHeight;
+  } else {
+   let sizeMap = {
+    small: [36, 12],
+    normal: [72, 24],
+    large: [108, 36],
+    xlarge: [144, 48],
+    xxlarge: [156, 60]
+   };
+   containerWith = sizeMap[props.size][0];
+   containerHeight = sizeMap[props.size][1];
 
+  }
 
-return `
-
-.battery_${id} {
-  background: ${props.background};
-  position: relative;
-  width: 36px;
-  height: 12px;
-  border: 1px solid black;
-  border-radius: 2px;
-  position: relative;
-  padding: 1px;
-  -webkit-transform: scale(0.5);
+  return {
+   width: containerWith,
+   height: containerHeight,
+   padding: parseInt(containerHeight / 12),
+   border: parseInt(containerHeight / 12)
+  };
+ },
+ sizeCoeffecient(containerHeight) {
+   let coeff =parseInt(containerHeight / 12);
+   if(!coeff) return 1;
+   return (coeff % 3);
+ },
+ getForeground(props, state) {
+  if (state.level && state.level < 10) {
+   return props.colorOnCritical;
+  } else {
+   return (!state.charging) ? props.color : props.colorOnCharing;
+  }
+ },
+ barWidth(props, state) {
+  return `${state.level}%`;
+ },
+ barBackgroundColor(props, state) {
+  return this.getForeground(...arguments);
+ }
 }
-.battery_${id}:before {
+const getStyleByPropsAndState = (props, state) => {
+ let style = {
+  container: {
+   $before: {},
+   $after: {},
+   $plugged: {}
+  },
+  bar: {}
+ };
+ style.container.background = props.background;
+ style.container = Object.assign(style.container, api.containerSize(props, state)); //width,height,border,padding
+ style.container.scale = api.containerScale(props, state);
+ style.container.borderColor = api.getForeground(props, state);
+ style.container.$before.sizeCoeff = api.sizeCoeffecient(style.container.height);
+
+ style.container.$before.background = api.getForeground(props, state);
+ style.container.$plugged.sizeCoeff = style.container.$before.sizeCoeff;
+ style.container.$plugged.background = api.getForeground(props, state);
+
+ style.bar.width = api.barWidth(props, state);
+ style.bar.background = api.barBackgroundColor(props, state);
+ return style;
+};
+module.exports = function(props, state) {
+
+ var style = getStyleByPropsAndState(props, state);
+ return `
+
+.battery_${state.id} {
+  background: ${style.container.background};
+  position: relative;
+  width: ${style.container.width}px;
+  height: ${style.container.height}px;
+  border: ${style.container.border}px solid ${style.container.borderColor};
+  border-radius: ${style.container.border*2}px;
+  position: relative;
+  padding: ${style.container.padding}px;
+  -webkit-transform: scale(${style.container.scale});
+  transform: scale(${style.container.scale});
+}
+.battery_${state.id}:before {
   content: '';
   display: block;
   position: absolute;
   top: 50%;
-  right: -5.1px;
-  margin-top: -3px;
-  width: 6px;
-  height: 6px;
-  background: ${props.foreground};
-  clip: rect(0, 6px, 6px, 3.9px);
+  right: -${5.1*style.container.$before.sizeCoeff}px;
+  margin-top: -${3*style.container.$before.sizeCoeff}px;
+  width: ${6*style.container.$before.sizeCoeff}px;
+  height: ${6*style.container.$before.sizeCoeff}px;
+  background: ${style.container.$before.background};
+  clip: rect(0, ${6*style.container.$before.sizeCoeff}px, ${6*style.container.$before.sizeCoeff}px, 3.9px);
   border-radius: 50%;
 }
-.battery_${id}.plugged:after {
+.battery_${state.id}.plugged:after {
   content: '';
   display: block;
   position: absolute;
   top: 50%;
   right: -19px;
-  margin-top: -3px;
-  width: 6px;
-  height: 6px;
-  background: ${props.foreground};
+  margin-top: -${3*style.container.$plugged.sizeCoeff}px;
+  width: ${6*style.container.$plugged.sizeCoeff}px;
+  height: ${6*style.container.$plugged.sizeCoeff}px;
+  background: ${style.container.$plugged.background};
   border-radius: 50%;
 }
-.battery_${id} .level_${id} {
-  width: 10%;
+.battery_${state.id} .level_${state.id} {
+  width: ${style.bar.width};
   height: 100%;
-  background: ${props.foreground};
+  background: ${style.bar.background};
   border-radius: 1px;
+}
+.fa-battery-4_${state.id}:before, .fa-battery_${state.id}:before, .fa-battery-full_${state.id}:before {
+  content:"\f240";
+ }
+ .fa-battery-0_${state.id}:before, .fa-battery-empty_${state.id}:before {
+    content: "\f244";
+}
+
+.fa-battery-1_${state.id}:before, .fa-battery-quarter_${state.id}:before {
+    content: "\f243";
+}
+.fa-battery-2_${state.id}:before, .fa-battery-half_${state.id}:before {
+    content: "\f242";
+}
+.fa-battery-3_${state.id}:before, .fa-battery-three-quarters_${state.id}:before {
+    content: "\f241";
 }
 `;
 
